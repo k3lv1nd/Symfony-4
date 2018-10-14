4faf1b08bac355e5997f801b5b555430acadce04
k3lv1nd <kalvind95@gmail.com>
2018-10-15 00:42:41 +0300

    contact form

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(Request $request, \Swift_Mailer $mailer)
+    {
+
+
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+       if($form->isSubmitted() && $form->isValid())
+       {
+           $contactFormData = $form->getData();
+           $message = (new \Swift_Message('You got mail'))
+               ->setFrom($contactFormData['email'])
+               ->setTo('kalvind95@gmail.com')
+               ->setBody(
+                   $contactFormData['message'], 'text/plain'
+               );
+           $mailer->send($message);
+       }
+       return $this->render('contact/contact.html.twig', [
+            'our_form' => $form->createView()
+        ]);
+
+
+    }
+}
--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class WelcomeController extends AbstractController
 {
@@ -14,5 +17,25 @@ class WelcomeController extends AbstractController
     public function index()
     {
         return $this->render('welcome/index.html.twig');
+    }
+
+    /**
+     * @Route("/hello/{name}",
+     *      name="hello_page",
+     * requirements={"name" = "[A-Za-z]+"}
+     * )
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response;
+     */
+    public function helloPage (Request $request)
+    {
+
+        $name = $request->query->get('name', 'kelvin');
+        return $this->render('hello_page.html.twig',
+            [
+                'some_variable' => 'I am the passed value',
+                'name' => $name
+            ]
+            );
     }
 }
